66a02a6efc474d7197068ab032a6f7c24d1524f2
Chintanneosoft <chintan.rajgor@neosoftmail.com>
2023-11-28 11:51:26 +0530

    Initial Commit

---
+++ SwiftUI Unit Testing/SwiftUI_Unit_TestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Unit_TestingApp.swift
+//  SwiftUI Unit Testing
+//
+//  Created by Neosoft on 28/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Unit_TestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
